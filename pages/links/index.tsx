9de181a3eab356a567fe0eb44cb50b0468dec02a
Cupid Valentine <westerngod@126.com>
2022-04-12 00:15:33 +0800

    add link css

--- pages/links/index.tsx
+++ pages/links/index.tsx
@@ -1,5 +1,59 @@
-import React from 'react'
+import { NextPage } from 'next'
+import React, { useEffect, useState } from 'react'
+import { Card, Row, Col, Button } from 'antd'
+import { LinkOutlined } from '@ant-design/icons'
+import { getLinks } from '@/api/common'
+import { HttpResponse, LinkInfo } from '@/common/interface'
+import Image from 'next/image'
 
-export default function Links() {
-  return <div>Links</div>
+import style from './links.module.scss'
+
+const Links: NextPage = () => {
+  const [links, setLinks] = useState<LinkInfo[]>([])
+  const getLinkLists = async () => {
+    const { code, msg, data } = (await getLinks()) as HttpResponse
+    if (code === 200) {
+      setLinks(data)
+    }
+  }
+
+  useEffect(() => {
+    getLinkLists()
+  })
+  return (
+    <div className={style.links + ' container mt-2'}>
+      <Card className="mb-2" title={'友链信息'}>
+        <p>站名：Valcosmos</p>
+        <p>地址：valzt.cn</p>
+        <p>头像：https://api.valzt.cn/media/avatar_me.png</p>
+        <p>个签：人生如逆旅，我亦是行人</p>
+      </Card>
+
+      <Row gutter={20}>
+        {links.map((link) => (
+          <Col key={link._id} xs={24} sm={12} md={8} lg={6} className="mb-2">
+            <Card
+              cover={
+                <Image
+                  alt="avatar"
+                  src={link.avatar}
+                  width={100}
+                  height={100}
+                />
+              }
+              actions={[
+                <Button type="primary" icon={<LinkOutlined />} key={'link'}>
+                  访问
+                </Button>
+              ]}
+            >
+              <Card.Meta title={link.nickname} description={link.desc} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  )
 }
+
+export default Links
