085cb02671f43f89bd3350f79955f9ede20ecc36
Cupid Valentine <westerngod@126.com>
2022-04-11 15:58:02 +0800

    add footer

---
+++ components/footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      Copyright © 2019-2022 valzt.cn 版权所有
+      <a href="http://beian.miit.gov.cn" target="_blank" rel="noreferrer">
+        鄂ICP备19028697号
+      </a>
+    </div>
+  )
+}
