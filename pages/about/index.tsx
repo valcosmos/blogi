b5dc504b0a35ac5ab498c062629c51cdb6894c3b
Cupid Valentine <westerngod@126.com>
2022-06-07 23:54:45 +0800

    add title in about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,4 +1,6 @@
 import { NextPage } from 'next'
+import Head from 'next/head'
+
 import React, { useEffect, useState } from 'react'
 import { Avatar } from 'antd'
 import Editor from 'md-editor-rt'
@@ -23,16 +25,22 @@ const About: NextPage = () => {
     getInfo()
   }, [])
   return (
-    <div className="container">
-      <div className={style.about + ' padding-2'}>
-        <div className="avatar">
-          <Avatar size={100} src={avatar}></Avatar>
-        </div>
-        <div className="content">
-          <Editor modelValue={text} previewOnly></Editor>
+    <>
+      <Head>
+        <title>Valcosmos | 关于我</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="container">
+        <div className={style.about + ' padding-2'}>
+          <div className="avatar">
+            <Avatar size={100} src={avatar}></Avatar>
+          </div>
+          <div className="content">
+            <Editor modelValue={text} previewOnly></Editor>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
