57e826ef4fa2d6a936a5796a0ce12b0d4fc71a2e
Cupid Valentine <westerngod@126.com>
2022-06-08 23:20:19 +0800

    add desc and keyword in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,16 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Valcosmos | 李青丘的个人博客</title>
+
+        <meta
+          name="description"
+          content="valcosmos-李青丘的个人博客-Web前端开发-分享前端技术"
+        />
+        
+        <meta name="keywords" content="HTML5, CSS3, JavaScript, TypeScript, Vue, React, Koa, nodejs, Jenkins, Docker, Golang, Gin, Python" />
+        
+        <meta name="author" content="Cupid Valentine | 李青丘" />
+
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="home container">
