fdcb013a2e3de64d5b53a1dc3ad3202b3db3cfec
Cupid Valentine <westerngod@126.com>
2022-06-09 23:24:25 +0800

    add dynamic in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
+
+import dynamic from 'next/dynamic'
+
+const Layout = dynamic(() => import('@/components/layout'))
+
+// import Layout from '../components/layout'
+
 import 'md-editor-rt/lib/style.css'
+
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
