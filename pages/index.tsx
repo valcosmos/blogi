fd93a4b342499e1d34826918b85c9aa66534e386
Cupid Valentine <westerngod@126.com>
2022-06-11 22:06:18 +0800

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 
 import Head from 'next/head'
 
 import dynamic from 'next/dynamic'
 
-import { Col, Row, Badge } from 'antd'
+import {Col, Row, Badge} from 'antd'
+import {useState} from "react";
 
 // const DynamicComponent = dynamic(() =>
 //   import('../components/hello').then((mod) => mod.Hello)
@@ -25,6 +26,12 @@ const Tags = dynamic(() => import('@/components/tags'))
 
 
 const Home: NextPage = () => {
+  const [tag, setTag] = useState<string>('')
+
+  const getTag = (tag: string) => {
+    setTag(tag)
+  }
+
   return (
     <>
       <Head>
@@ -40,18 +47,18 @@ const Home: NextPage = () => {
           content="HTML5, CSS3, JavaScript, TypeScript, Vue, React, Koa, nodejs, Jenkins, Docker, Golang, Gin, Python"
         />
 
-        <meta name="author" content="Cupid Valentine | 李青丘" />
+        <meta name="author" content="Cupid Valentine | 李青丘"/>
 
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
       </Head>
       <div className="home container">
         <Row gutter={20}>
           <Col xs={24} sm={24} md={18} lg={18}>
-            <Posts />
+            <Posts tag={tag} />
           </Col>
           <Col xs={24} sm={24} md={6} lg={6}>
-            <Hots />
-            <Tags />
+            <Hots/>
+            <Tags getTag={getTag}/>
           </Col>
         </Row>
       </div>
