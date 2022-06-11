4237556f54fba850085e6db16ffd03e9873b7021
Cupid Valentine <westerngod@126.com>
2022-06-11 21:46:57 +0800

    update layout

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,6 +1,6 @@
-import { FC, ReactNode } from 'react'
+import {FC, ReactNode} from 'react'
 // import style from './layout.module.scss'
-import { Layout } from 'antd'
+import {Layout} from 'antd'
 
 import Menu from '@/components/menu'
 
@@ -8,23 +8,31 @@ import Banner from '@/components/banner'
 
 import Footer from '@/components/footer'
 
+import style from './layout.module.scss'
+import {useRouter} from "next/router";
+
 interface LayoutProps {
   children?: ReactNode
 }
 
-const LayoutCom: FC<LayoutProps> = ({ children }) => {
+const LayoutCom: FC<LayoutProps> = ({children}) => {
+
+  const router = useRouter()
+
+  console.log(typeof router.pathname)
   return (
     <div>
-      <Layout className="l-layout">
+      <Layout className={style.layout}>
         <Layout.Header className="l-header">
-          <Menu />
+          <Menu/>
         </Layout.Header>
 
         <Layout.Content>
-          <div className="banner">
-            <Banner />
-          </div>
-          {/* <main>{children}</main> */}
+          {!router.pathname.includes('post') && <div className="banner">
+            <Banner/>
+          </div>}
+
+
           {children}
         </Layout.Content>
 
