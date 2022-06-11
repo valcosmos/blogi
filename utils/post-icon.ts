b493460a4788217589019eda7f20c231b6e2d76d
Cupid Valentine <westerngod@126.com>
2022-06-11 22:06:37 +0800

    add post icon

---
+++ utils/post-icon.ts
@@ -0,0 +1,57 @@
+export interface postIconProps {
+  tag: string
+  icon: string
+}
+
+export const postIcon: postIconProps[] = [
+  {
+    tag: 'Vue',
+    icon: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg'
+  },
+  {
+    tag: 'React',
+    icon: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg'
+  },
+  {
+    tag: 'webpack',
+    icon: 'https://webpack.js.org/assets/icon-square-big.svg'
+  },
+  {
+    tag: 'node',
+    icon: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg'
+  },
+  {
+    tag: 'koa',
+    icon: 'https://github.com/koajs/koa/raw/master/docs/logo.png'
+  },
+  {
+    tag: 'express',
+    icon: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg'
+  },
+  { tag: 'Django', icon: 'https://simpleicons.org/icons/django.svg' },
+  {
+    tag: 'Python',
+    icon: 'https://avatars.githubusercontent.com/u/1525981?s=200&v=4'
+  },
+  {
+    tag: 'Go',
+    icon: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg'
+  },
+  {
+    tag: 'Gin',
+    icon: 'https://raw.githubusercontent.com/gin-gonic/logo/master/color.png'
+  },
+  { tag: 'Beego', icon: 'https://beego.vip/static/img/beego_purple.png' },
+  {
+    tag: 'TypeScript',
+    icon: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg'
+  },
+  {
+    tag: 'JavaScript',
+    icon: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg'
+  },
+  {
+    tag: 'Jenkins',
+    icon: 'https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg'
+  }
+]
