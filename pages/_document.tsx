9dffd0402d1047e6093b61e72f5c0f335fb89153
Cupid Valentine <westerngod@126.com>
2022-06-27 23:13:48 +0800

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,13 +2,26 @@ import {Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang={'zh'}>
-      <Head>
-        <title>Valcosmos | 李青丘的个人博客</title>
-      </Head>
+    <Html>
+      <Head/>
       <body>
       <Main/>
       <NextScript/>
+
+
+      {/*// Global site tag (gtag.js) - Google Analytics */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-GR1N2C17YX"/>
+      <script dangerouslySetInnerHTML={{
+        __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GR1N2C17YX');
+            `
+      }}/>
+
+
       </body>
     </Html>
   )
