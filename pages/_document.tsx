b4e6dca305765393af9f9e9958a77b29021964d8
Cupid Valentine <westerngod@126.com>
2022-06-27 23:42:46 +0800

    fix google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,15 @@
 import {Html, Head, Main, NextScript} from 'next/document'
-import Script from 'next/script'
+// import Script from 'next/script'
 export default function Document() {
   return (
     <Html>
       <Head>
 
         {/*// Global site tag (gtag.js) - Google Analytics */}
-        <Script id={'gtag-js'}  strategy="afterInteractive" src={'https://www.googletagmanager.com/gtag/js?id=G-GR1N2C17YX'} />
-        {/*<script async src="https://www.googletagmanager.com/gtag/js?id=G-GR1N2C17YX"/>*/}
-        <Script id={'analytics-code'} strategy="afterInteractive" dangerouslySetInnerHTML={{
+        {/*<Script id={'gtag-js'}  strategy="afterInteractive" src={'https://www.googletagmanager.com/gtag/js?id=G-GR1N2C17YX'} />*/}
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GR1N2C17YX"/>
+        <script dangerouslySetInnerHTML={{
           __html: `
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
