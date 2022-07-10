2917a120dc95a62f80f15e323595f5b643cc084a
Cupid Valentine <westerngod@126.com>
2022-07-10 23:39:50 +0800

    fix error

--- components/posts/index.tsx
+++ components/posts/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 import {List, message, Space} from 'antd'
 
@@ -96,6 +96,10 @@ export default function PostList({tag, postList, postTotal}: { tag: string, post
   useUpdateEffect(() => {
     getPostList()
   }, [pageInfo.current, tag])
+
+  useEffect(() => {
+    setList(postList.map((item:any) => ({...item, created: formatDate(item.created)})))
+  }, [postList])
 
   const icons: Record<string, any | string>[] = [
     {type: EyeOutlined, text: 'read'},
