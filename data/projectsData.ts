2957e5b57b60fec528ea0949c8c52686c61374ec
Cupid Valentine <westerngod@126.com>
2024-09-29 23:39:37 +0800

    feat: init project

---
+++ data/projectsData.ts
@@ -0,0 +1,17 @@
+interface Project {
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'xxx',
+    description: `xxx`,
+    imgSrc: '',
+    href: 'https://www.github.com/valcosmos',
+  },
+]
+
+export default projectsData
