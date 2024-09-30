64eb7d66311fbfa7730db6fb8e7888135a1a95f0
Cupid Valentine <westerngod@126.com>
2024-09-30 23:48:35 +0800

    fixed

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -14,33 +14,33 @@ export default function AuthorLayout({ children, content }: Props) {
   return (
 
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
-      <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           关于我
         </h1>
       </div>
       <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-        <div className="flex flex-col items-center pt-8 space-x-2">
+        <div className="flex flex-col items-center space-x-2 pt-8">
           {avatar && (
             <Image
               src={avatar}
               alt="avatar"
               width={192}
               height={192}
-              className="rounded-full size-48"
+              className="size-48 rounded-full"
             />
           )}
-          <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
+          <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
           <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
           <div className="text-gray-500 dark:text-gray-400">{company}</div>
-          <div className="flex pt-6 space-x-3">
+          <div className="flex space-x-3 pt-6">
             <SocialIcon kind="mail" href={`mailto:${email}`} />
             <SocialIcon kind="github" href={github} />
             <SocialIcon kind="linkedin" href={linkedin} />
             <SocialIcon kind="x" href={twitter} />
           </div>
         </div>
-        <div className="py-8 prose max-w-none dark:prose-invert xl:col-span-2">
+        <div className="prose max-w-none py-8 dark:prose-invert xl:col-span-2">
           {children}
         </div>
       </div>
